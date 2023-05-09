5d1fe09cdbc3ea46671648e0daddf58b51fec880
samuel-lima-ribeiro <samuel.ribeiro@sptech.school>
2023-05-09 00:14:12 -0300

    sla

--- script.sh
+++ script.sh
@@ -4,6 +4,7 @@ sudo docker pull mysql:5.7
 sudo docker stop $(sudo docker ps -aq)
 sudo docker rm $(sudo docker ps -aq)
 sudo docker run -d -p 3306:3306 --name ContainerBD -e "MYSQL_DATABASE=ToodDatabase" -e "MYSQL_ROOT_PASSWORD=sptech" mysql:5.7
-sudo docker exec -it ContainerBD bash -c "mysql -u root -p"
+sudo docker exec -it ContainerBD bash
+mysql -u root -p
 chmod 777 login-tood-java-swing-1.0-SNAPSHOT-jar-with-dependencies.jar
 java -jar login-tood-java-swing-1.0-SNAPSHOT-jar-with-dependencies.jar
