2c69abf08163320283944f02e5b6288acc15f963
samuel-lima-ribeiro <samuel.ribeiro@sptech.school>
2023-05-08 21:05:29 -0300

    aaaaaaaa

--- script.sh
+++ script.sh
@@ -1,3 +1,13 @@
+SQL="
+CREATE TABLE IF NOT EXISTS `ToodDatabase`.`DadoTotem` (
+  `idDadosTotem` INT NOT NULL AUTO_INCREMENT,
+  `dataHora` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+  `qtdRam` varchar(100),
+  `qtdDisco` varchar(100),
+  `qtdProcessador` varchar(100),
+  PRIMARY KEY (`idDadosTotem`)
+);
+"
 sudo apt update && sudo apt upgrade –y
 sudo apt install docker.io
 sudo docker pull mysql:5.7
@@ -5,6 +15,6 @@ sudo docker stop $(sudo docker ps -aq)
 sudo docker rm $(sudo docker ps -aq)
 sudo docker run -d -p 3306:3306 --name ContainerBD -e "MYSQL_DATABASE=ToodDatabase" -e "MYSQL_ROOT_PASSWORD=sptech" mysql:5.7
 sudo docker exec -it ContainerBD bash
-mysql –u root -psptech -a -f script.sql
+mysql –u root -psptech <<< $SQL
 chmod 777 login-tood-java-swing-1.0-SNAPSHOT-jar-with-dependencies.jar
 java -jar login-tood-java-swing-1.0-SNAPSHOT-jar-with-dependencies.jar
