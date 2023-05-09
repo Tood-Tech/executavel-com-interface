850a3105690a5e0d7fc8c531bbd9039378871026
samuel-lima-ribeiro <samuel.ribeiro@sptech.school>
2023-05-09 00:05:03 -0300

    teste

--- script.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `ToodDatabase`.`DadoTotem` (
-  `idDadosTotem` INT NOT NULL AUTO_INCREMENT,
-  `dataHora` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
-  `qtdRam` varchar(100),
-  `qtdDisco` varchar(100),
-  `qtdProcessador` varchar(100),
-  PRIMARY KEY (`idDadosTotem`)
-);
