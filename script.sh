16d51a0a2211538dccf89ca75213dded55df88dc
samuel-lima-ribeiro <samuel.ribeiro@sptech.school>
2023-05-08 21:45:17 -0300

    sla

--- script.sh
+++ script.sh
@@ -1,11 +1,11 @@
 SQL="\
-CREATE TABLE IF NOT EXISTS `ToodDatabase`.`DadoTotem` (\
-  `idDadosTotem` INT NOT NULL AUTO_INCREMENT,\
-  `dataHora` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,\
-  `qtdRam` varchar(100),\
-  `qtdDisco` varchar(100),\
-  `qtdProcessador` varchar(100),\
-  PRIMARY KEY (`idDadosTotem`)\
+CREATE TABLE IF NOT EXISTS "ToodDatabase"."DadoTotem" (\
+  "idDadosTotem" INT NOT NULL AUTO_INCREMENT,\
+  "dataHora" DATETIME NULL DEFAULT CURRENT_TIMESTAMP,\
+  "qtdRam" varchar(100),\
+  "qtdDisco" varchar(100),\
+  "qtdProcessador" varchar(100),\
+  PRIMARY KEY ("idDadosTotem")\
 );\
 "
 sudo apt update && sudo apt upgrade –y
