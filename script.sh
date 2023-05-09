f26d7805a474af17ab5de0304d163d8e8e59db3a
samuel-lima-ribeiro <samuel.ribeiro@sptech.school>
2023-05-08 21:40:42 -0300

    teste

--- script.sh
+++ script.sh
@@ -1,12 +1,12 @@
-SQL="
-CREATE TABLE IF NOT EXISTS `ToodDatabase`.`DadoTotem` (
-  `idDadosTotem` INT NOT NULL AUTO_INCREMENT,
-  `dataHora` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
-  `qtdRam` varchar(100),
-  `qtdDisco` varchar(100),
-  `qtdProcessador` varchar(100),
-  PRIMARY KEY (`idDadosTotem`)
-);
+SQL="\
+CREATE TABLE IF NOT EXISTS `ToodDatabase`.`DadoTotem` (\
+  `idDadosTotem` INT NOT NULL AUTO_INCREMENT,\
+  `dataHora` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,\
+  `qtdRam` varchar(100),\
+  `qtdDisco` varchar(100),\
+  `qtdProcessador` varchar(100),\
+  PRIMARY KEY (`idDadosTotem`)\
+);\
 "
 sudo apt update && sudo apt upgrade –y
 sudo apt install docker.io
